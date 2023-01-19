9e895ea82d9adc28c3b5ae584422ce6359e59352
Maxim Kvasnikov <mkvasnikov@vtb.ru>
2023-01-19 10:42:01 +0300

    Initial Commit

---
+++ QR Cash/QR_CashApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_CashApp.swift
+//  QR Cash
+//
+//  Created by Квасников Максим Геннадьевич on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct QR_CashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

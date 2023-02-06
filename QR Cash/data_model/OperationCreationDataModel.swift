9d4cfbe0283ee3f7560b67b8264722c9be8c984a
Maxim Kvasnikov <mkvasnikov@vtb.ru>
2023-02-06 10:30:50 +0300

    operation creation data model

---
+++ QR Cash/data_model/OperationCreationDataModel.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+
+class OperationCreationDataModel: ObservableObject, Statused {
+    
+    @Published private(set) var orderId: String?
+    @Published private(set) var status: DataModelStatus = .initializing
+    
+    
+    func createOrder(request: WithdrawalOperationRequest, sessionData: SessionData) {
+        guard status == .initializing else { return }
+        status = .loading
+        
+        let _ = qrCashService.createOperation(operationRequest: request, sessionData: sessionData)
+            .onResult(handleOperationCreate)
+            .onError(handleRequestError)
+    }
+    
+    private func handleOperationCreate(response: OperationResponse) {
+        if response.success && response.orderId != nil {
+            status = .done
+            orderId = response.orderId
+        } else {
+            status = .error
+        }
+    }
+    
+    private func handleRequestError(error: Error) {
+        status = .error
+    }
+}
